65581cef1ed5fcc571c61aeedc1b3cc9e0987386
girmaaa <ritagirmab@gmail.com>
2024-09-06 08:14:14 -0700

    Initial commit with basic structure

---
+++ README.md
@@ -0,0 +1,3 @@
+# foonathan_memory
+
+This is a placeholder repository for the foonathan_memory project. Add your description here.
---
+++ include/foonathan_memory.h
@@ -0,0 +1,6 @@
+#ifndef FOONATHAN_MEMORY_H
+#define FOONATHAN_MEMORY_H
+
+// Placeholder header file
+
+#endif // FOONATHAN_MEMORY_H
